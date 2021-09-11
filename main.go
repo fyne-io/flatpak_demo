a1ea8084803760f96b06f93aa4fdff7d053ab1d6
Jacob Alzen <jacob.alzen@gmail.com>
2021-09-11 13:06:55 +0200

    Filter to only open markdown files

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
@@ -21,7 +22,7 @@ func main() {
 	markdown.OnChanged = preview.ParseMarkdown
 
 	open := &widget.Button{Text: "Open file", Icon: theme.ContentAddIcon(), OnTapped: func() {
-		dialog.ShowFileOpen(func(file fyne.URIReadCloser, err error) {
+		files := dialog.NewFileOpen(func(file fyne.URIReadCloser, err error) {
 			if err != nil {
 				log.Println(err)
 				return
@@ -37,6 +38,8 @@ func main() {
 
 			markdown.SetText(string(text))
 		}, w)
+		files.SetFilter(storage.NewExtensionFileFilter([]string{".md"}))
+		files.Show()
 	}}
 
 	w.SetContent(
